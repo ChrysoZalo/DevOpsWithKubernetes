e93088f4de99d0e1f4eb67ec3332cf604ef6af69
czaloumis <czaloumis@impedimed.com>
2024-10-06 04:00:08 +0300

    Final of Part1

--- todo_app/main.go
+++ todo_app/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io"
 	"net/http"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -19,16 +22,25 @@ type TodoPageData struct {
 	Todos     []Todo
 }
 
+var mu sync.Mutex // Mutex to protect concurrent writes to the file
+
 func main() {
 	r := mux.NewRouter()
 
+	todoRouter := r.PathPrefix("/todo").Subrouter()
+
 	tmpl := template.Must(template.ParseFiles("layout.html"))
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+	todoRouter.PathPrefix("/tmp/").Handler(http.StripPrefix("/todo/tmp/", http.FileServer(http.Dir("/tmp/"))))
+	todoRouter.HandleFunc("", func(w http.ResponseWriter, r *http.Request) {
 		ListTodo(tmpl, w, r)
 	}).Methods("GET")
 
-	port := os.Getenv("PORT")
-	// port := "80" For testing
+	// Start the background task to download the image every 60 seconds
+	go startImageFetcher()
+
+	// Start the server
+	port := "80" // For testing
 	fmt.Printf("Server started in port %s\n", port)
 	http.ListenAndServe(":"+port, r)
 }
@@ -46,4 +58,46 @@ func ListTodo(t *template.Template, w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+// startImageFetcher fetches a new image every 60 seconds
+func startImageFetcher() {
+	ticker := time.NewTicker(60 * time.Minute)
+	defer ticker.Stop()
+
+	for {
+		fetchAndSaveImage()
+
+		<-ticker.C // Wait for the next tick (60 seconds)
+	}
+}
+
+func fetchAndSaveImage() {
+	fmt.Println("Fetching new image...")
+
+	resp, err := http.Get("https://picsum.photos/200")
+	if err != nil {
+		fmt.Printf("Error downloading image: %v\n", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Use a mutex to prevent concurrent writes to the file
+	mu.Lock()
+	defer mu.Unlock()
+
+	file, err := os.Create("/tmp/images/image.jpg")
+	if err != nil {
+		fmt.Printf("Error creating image file: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		fmt.Printf("Error saving image: %v\n", err)
+		return
+	}
+
+	fmt.Println("Image successfully saved.")
 }
